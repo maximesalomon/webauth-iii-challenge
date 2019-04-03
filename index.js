5883b4564306025d45f52b8335030e6f60813253
Maxime Salomon <maxime@croissant.io>
2019-04-03 14:10:48 +0200

    added middleware + bcrypt + jsonwebtoken

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const db = require("./data/dbConfig");
+const mw = require("./middleware");
 
 const server = express();
 
