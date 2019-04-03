2f2c2f28141c029e3d92b91dd7d906a606cc2edd
Maxime Salomon <maxime@croissant.io>
2019-04-03 14:11:22 +0200

    added POST Register

--- index.js
+++ index.js
@@ -12,6 +12,34 @@ server.use(express.json(), cors());
 
 // ENDPOINTS
 
+// POST	/api/register
+
+server.post("/api/register", (req, res) => {
+    const creds = req.body;
+    if (mw.passCheck(creds)) {
+      creds.password = bcrypt.hashSync(creds.password, 12);
+      db.addUser(creds)
+        .then(ids => {
+          const id = ids[0];
+          db.findUserByID(id)
+            .then(user => {
+              const token = mw.generateToken(user);
+              res.status(201).json({ id: user.id, token });
+            })
+            .catch(err => res.status(500).send(err));
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ err: `There was an error adding that user: ${err}` });
+        });
+    } else {
+      res
+        .status(406)
+        .send("Please structure your password according to the proper rules");
+    }
+});
+
 server.get('/*', (req, res) => {
     res.send('Hello world!');
 });
